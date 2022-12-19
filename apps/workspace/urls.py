f6d5e386c65449357fa3ebe21487393cd6874412
David Acosta <acostafajardodavid@gmail.com>
2022-12-19 04:03:29 -0700

    Container workspaces css

--- apps/workspace/urls.py
+++ apps/workspace/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from apps.workspace import views
 urlpatterns = [
-    path('',views.Home.as_view())
+    path('',views.Home.as_view(),name='home'),
+    path('boards',views.ListsWorkspaces.as_view(),name='home_boards')
 ]
--- apps/workspace/views.py
+++ apps/workspace/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView,ListView
-from apps.workspace import models
+from apps.workspace.models import Workspace
+from apps.board.models import Board
 # Create your views here.
 
-# class Home(TemplateView):
-#     template_name = 'home.html'
+class Home(TemplateView):
+    template_name = 'home_page/home.html'
 
 class ListsWorkspaces(ListView):
-    model = models.Workspace
+    model = Workspace
     template_name = 'home_boards/home_boards.html'
 
-    def get_query_set(self):
+    def get(self,request):
+        list_boards = []
         user = self.request.user
-        query = self.model.objects()
+        workspaces = Workspace.objects.filter(created_by = user)
+        for workspace in workspaces:
+            list_boards.append(Board.objects.filter(workspace=workspace))
+        print(list_boards)
+        context = {
+            'workspaces':workspaces,
+            'list_workspaces':list_boards,
+        }
+        return render(request,'home_page/home_boards.html',context)
